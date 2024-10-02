a142cf4f98ab50311652bb3c5fee17c64523818d
pshivkum@cisco.com <pshivkum@cisco.com>
2024-10-02 18:59:02 -0400

    clean and modular code

--- main.py
+++ main.py
@@ -1,73 +1,103 @@
-# version account_id interface_id srcaddr dstaddr srcport dstport protocol packets bytes start end action log_status vpc-id subnet-id instance-id tcp-flags
 import csv
+import sys
 
-def load_protocol_lookup(filename: str) -> dict:
-    protocol = {}
-    with open(filename, newline="", mode="r") as f:
-        reader = csv.DictReader(f)
-        try:
+
+class ProtocolLookup:
+    def __init__(self, filename: str):
+        self.protocol = self.load("protocol-map.csv")
+
+    def load(self, filename: str) -> dict:
+        protocol = {}
+        with open(filename, newline="", mode="r") as file:
+            reader = csv.DictReader(file)
             for row in reader:
                 if row["Decimal"] == "146-252":
                     for p in range(146, 253):
                         protocol[p] = "reserved"
-                    continue
-                protocol[int(row["Decimal"])] = row["Keyword"].lower()
-        except KeyError as e:
-            print(f"Error: {e}")
-    return protocol
+                else:
+                    protocol[int(row["Decimal"])] = row["Keyword"].lower()
 
-def load_lookup(filename: str) -> dict:
-    lookup = {}
-    with open(filename, newline="", mode="r") as f:
-        reader = csv.DictReader(f)
-        try:
-            for row in reader:
-                dstport = int(row["dstport"])
-                protocol = row["protocol"]
-                tag = row["tag"]
-                lookup[(dstport, protocol)] = tag
-        except KeyError as e:
-            print(f"Error: {e}")
-    return lookup
+        return protocol
 
-def process_flow_logs(log_file: str, lookup: dict, protocol_map: dict) -> dict:
-    tag_mappings = {key: 0 for key in lookup.values()}
-    tag_mappings["Untagged"] = 0
-    print(tag_mappings)
-    with open(log_file, newline="", mode="r") as f:
-        lines = f.readlines()
-
-        for line in lines:
-            fields = line.split()
-            dstport = int(fields[5])
-            protocol = protocol_map.get(int(fields[7]))
-            if (dstport, protocol) in lookup:
-                tag = lookup[(dstport, protocol)]
-                print(f"Tag: {tag}")
-                tag_mappings[tag] += 1
-            else:
-                print("No tag found")
-                tag_mappings["Untagged"] += 1
+    def get_string(self, protocol: int) -> str:
+        return self.protocol.get(protocol, "unknown")
 
 
+class Lookup:
+    def __init__(self, filename: str):
+        self.lookup_map = self.load(filename)
 
-    return tag_mappings
+    def load(self, filename: str) -> dict:
+        lookup = {}
+        with open(filename, newline="", mode="r") as f:
+            reader = csv.DictReader(f)
+            try:
+                for row in reader:
+                    dstport = int(row["dstport"])
+                    protocol = row["protocol"]
+                    tag = row["tag"]
+                    lookup[(dstport, protocol)] = tag
+            except KeyError as e:
+                print(f"Failed to load lookup: {e}")
+                sys.exit(1)
 
-def write_to_output(output_file: str, tag_mappings: dict) -> None:
-    with open(output_file, newline="", mode="w") as f:
-        f.write("Tag Counts:\n\n")
-        for tag, count in tag_mappings.items():
-            f.write(f"{tag}, {count}\n")
+        return lookup
 
-        f.write("\nPort/Protocol Combination Counts:\n")
+    def get_tag(self, dstport: int, protocol: str) -> str:
+        return self.lookup_map.get((dstport, protocol), "Untagged")
+
+
+class LogProcessor:
+    def __init__(self, log_file: str, lookup: Lookup, protocol: ProtocolLookup):
+        self.log_file = log_file
+        self.lookup = lookup
+        self.protocol = protocol
+        self.tag_mappings = {
+            **dict.fromkeys(self.lookup.lookup_map.values(), 0),
+            "Untagged": 0,
+        }
+        self.port_protocol_counts = {}
+
+    def process_flow_logs(self) -> (dict, dict):
+        with open(self.log_file, newline="", mode="r") as file:
+            for line in file.readlines():
+                fields = line.split()
+                dstport = int(fields[5])
+
+                protocol_number = int(fields[7])
+                protocol = self.protocol.get_string(protocol_number)
+
+                tag = self.lookup.get_tag(dstport, protocol)
+
+                # Update the tag mappings and port protocol counts
+                self.tag_mappings[tag] += 1
+                self.port_protocol_counts[(dstport, protocol)] = (
+                    self.port_protocol_counts.get((dstport, protocol), 0) + 1
+                )
+
+        return self.tag_mappings, self.port_protocol_counts
+
+
+class OutputWriter:
+    @staticmethod
+    def write_to_output(
+        output_file: str, tag_mappings: dict, port_protocol: dict
+    ) -> None:
+        with open(output_file, newline="", mode="w") as file:
+            file.write("Tag Counts:\n\n")
+            for tag, count in tag_mappings.items():
+                if count > 0:
+                    file.write(f"{tag},{count}\n")
+
+            file.write("\nPort/Protocol Combination Counts:\n\n")
+
+            for (port, protocol), count in port_protocol.items():
+                file.write(f"{port},{protocol},{count}\n")
 
 
 if __name__ == "__main__":
-    protocol_lookup = load_protocol_lookup("protocol-map.csv")
-    print(protocol_lookup)
-    lookup = load_lookup("lookup.csv")
-    print(lookup)
-    tag_mappings = process_flow_logs("input.txt", lookup, protocol_lookup)
-    print(tag_mappings)
-    write_to_output("output.txt", tag_mappings)
-
+    protocol_lookup = ProtocolLookup("protocol-map.csv")
+    lookup = Lookup("lookup.csv")
+    processor = LogProcessor("input.txt", lookup, protocol_lookup)
+    tag_mappings, port_protocol_counts = processor.process_flow_logs()
+    OutputWriter.write_to_output("output.txt", tag_mappings, port_protocol_counts)
