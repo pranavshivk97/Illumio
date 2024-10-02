fab7f1b19f9caefbd4461ebbd3b939f7d2333225
pshivkum@cisco.com <pshivkum@cisco.com>
2024-10-02 17:47:43 -0400

    add tag mappings and generate output file

--- main.py
+++ main.py
@@ -16,6 +16,38 @@ def load_lookup(filename: str) -> dict:
             print(f"Error: {e}")
     return lookup
 
+def process_flow_logs(log_file: str, lookup: dict) -> dict:
+    tag_mappings = {key: 0 for key in lookup.values()}
+    tag_mappings["Untagged"] = 0
+    print(tag_mappings)
+    with open(log_file, newline="", mode="r") as f:
+        lines = f.readlines()
+
+        for line in lines:
+            fields = line.split()
+            dstport = int(fields[5])
+            protocol = int(fields[7])
+            if (dstport, protocol) in lookup:
+                tag = lookup[(dstport, protocol)]
+                print(f"Tag: {tag}")
+                tag_mappings[tag] += 1
+            else:
+                print("No tag found")
+                tag_mappings["Untagged"] += 1
+
+    return tag_mappings
+
+def write_to_output(output_file: str, tag_mappings: dict) -> None:
+    with open(output_file, newline="", mode="w") as f:
+        f.write("Tag Counts:\n")
+        for tag, count in tag_mappings.items():
+            f.write(f"{tag}, {count}\n")
+
 
 if __name__ == "__main__":
     lookup = load_lookup("lookup.csv")
+    print(lookup)
+    tag_mappings = process_flow_logs("input.txt", lookup)
+    print(tag_mappings)
+    write_to_output("output.txt", tag_mappings)
+
