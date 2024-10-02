62757eaf43ec738f920360ebaea01a6eb8e3d4d7
pshivkum@cisco.com <pshivkum@cisco.com>
2024-10-02 17:21:03 -0400

    load lookup

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-if __name__ =='__main__':
-    pass
+# version account_id interface_id srcaddr dstaddr srcport dstport protocol packets bytes start end action log_status vpc-id subnet-id instance-id tcp-flags
+import csv
+
+
+def load_lookup(filename: str) -> dict:
+    lookup = {}
+    with open(filename, newline="", mode="r") as f:
+        reader = csv.DictReader(f)
+        try:
+            for row in reader:
+                dstport = int(row["dstport"])
+                protocol = int(row["protocol"])
+                tag = row["tag"]
+                lookup[(dstport, protocol)] = tag
+        except KeyError as e:
+            print(f"Error: {e}")
+    return lookup
+
+
+if __name__ == "__main__":
+    lookup = load_lookup("lookup.csv")
